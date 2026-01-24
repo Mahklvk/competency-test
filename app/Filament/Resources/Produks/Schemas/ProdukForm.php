00a94715daf4d1ab1fec9d497706a1d8b54010f6
Mahklvk <ranggasidikp@proton.me>
2026-01-24 07:11:02 +0700

    minor changes for produk form

--- app/Filament/Resources/Produks/Schemas/ProdukForm.php
+++ app/Filament/Resources/Produks/Schemas/ProdukForm.php
@@ -7,8 +7,10 @@ use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Schemas\Components\Fieldset;
 use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
 
 class ProdukForm
 {
@@ -19,40 +21,44 @@ class ProdukForm
                 Fieldset::make('Informasi Produk')
                     ->columnSpanFull()
                     ->columns([
-                        'default' => 2,
-                        'md' => 1,
+                        'default' => 1,
+                        'md' => 2,
                     ])
                     ->schema([
                         TextInput::make('name')->required()->maxLength(225),
-                        TextInput::make('price')->prefix('Rp')->required()->numeric(),
+                        TextInput::make('price')->prefix('IDR')->required()->numeric(),
                         FileUpload::make('thumbnail')->directory('produk-thumbnail')->image()->imageEditor()->previewAble(true)->required(),
                         Repeater::make('Photos')->required()
+                            ->relationship('photos')
                             ->schema([
                                 FileUpload::make('photo')->directory('produk-photos')->image()->imageEditor()->previewAble(true)->columnSpanFull(),
 
                             ]),
 
                         Repeater::make('sizes')->required()
+                            ->relationship('sizes')
                             ->schema([
                                 TextInput::make('size')->required()->maxLength(225),
                             ]),
 
                         Fieldset::make('informasi tambahan')->columnSpanFull()
                             ->columns([
-                                'default' => 2,
-                                'md' => 1,
+                                'default' => 1,
+                                'md' => 2,
                             ])
                             ->schema([
                                 Textarea::make('about')->required(),
-                                Select::make('is_popular')->required()->options([
-                                    true => 'true',
-                                    false => 'false',
-                                ]),
+                                Toggle::make('is_popular')
+                                    ->onIcon(HeroIcon::Fire)
+                                    ->onColor('danger')
+                                    ->required()
+                                    ->inline(false)
+                                    ->label('Apakah Popular?'),
                                 Select::make('category_id')->required()
                                     ->relationship('category', 'name'),
                                 Select::make('brand_id')->required()
                                     ->relationship('brand', 'name'),
-                                TextInput::make('stock')->required()->prefix('Pcs'),
+                                TextInput::make('stock')->required()->prefix('Pcs')->columnSpanFull(),
                             ]),
                     ]),
             ]);
