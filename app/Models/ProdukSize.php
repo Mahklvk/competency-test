753533d2debb7ccf57a6d1bf7ce00a21cd36c7e2
Mahklvk <ranggasidikp@proton.me>
2026-01-24 07:16:07 +0700

    added relation to produk and producttransaction

--- app/Models/ProdukSize.php
+++ app/Models/ProdukSize.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProdukSize extends Model
@@ -14,4 +15,19 @@ class ProdukSize extends Model
         'size',
         'produk_id',
     ];
+
+    public function sizes(): BelongsTo
+    {
+        return $this->hasMany(Produk::class, 'size');
+    }
+
+    public function produk(): BelongsTo
+{
+    return $this->belongsTo(Produk::class, 'produk_id');
+}
+
+public function transactions(): HasMany
+{
+    return $this->hasMany(ProductTransaction::class, 'produk_size');
+}
 }
