69cdaec0a330bc644a94e76a39453c2f15212e47
Mahklvk <ranggasidikp@proton.me>
2026-01-24 07:16:27 +0700

    remove role

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,9 +20,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        
         'password',
-    ];
+        ];
 
     /**
      * The attributes that should be hidden for serialization.
