1ec1acde74e80443b0d6a837e12da1ef196ececb
Mahklvk <ranggasidikp@proton.me>
2026-01-24 07:17:09 +0700

    seeder for database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\PromoCode;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +21,31 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        User::create([
+            'name' => 'admin',
+            'email' => 'admintoko_ku@gmail.com',
+            'password' => Hash::make('admin123')
         ]);
+
+        Brand::create([
+            'name' => 'Nike',
+            'logo' => 'Nike.jpg'
+        ]);
+
+        Category::create([
+            'name' => 'Baju',
+            'icon' => 'Baju.jpg'
+        ]);
+
+        PromoCode::create([
+            'code' => 'AKUINGINKAYA',
+            'discount_amount' => 100000
+        ]);
+        
     }
 }
